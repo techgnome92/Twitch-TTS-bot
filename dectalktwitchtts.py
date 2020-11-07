b05d99c0ae2c921d4477f250c57b30101cfa5394
Frode <frodeluren@gmail.com>
2020-11-07 20:24:09 +0100

    Implements queue mode

--- dectalktwitchtts.py
+++ dectalktwitchtts.py
@@ -9,6 +9,10 @@ nickname = '' #The bots nickname
 token = 'oauth:' #Get it from https://twitchapps.com/tmi/
 channel = '#' #Channel username like '#moistcr1tikal'
 
+# 'keepup'  Keeping up with recent messages, some messages might be skipped.
+# 'queue'   Queuing up messages and says every message.
+MODE = 'queue'
+
 TMP_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tmp')
 if not os.path.exists(TMP_DIR):
     os.makedirs(TMP_DIR)
@@ -22,28 +26,52 @@ sock.send(f"JOIN {channel}\n".encode('utf-8'))
 resp = sock.recv(2048).decode('utf-8')
 resp = sock.recv(2048).decode('utf-8')
 
-while True:
-    try:
-        resp = sock.recv(2048).decode('utf-8')
-        print(resp)
-        message = resp.split(':',2)[2]
-        session = str(uuid.uuid1())
-        temp_wav = os.path.join(TMP_DIR, session+'.wav')
 
-        message = message.replace('"', '').replace('&', '').replace('|', '').replace(';', '')
 
-        if os.name == 'nt':
-            print("say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
-            os.system("say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
-        else:
-            print("wine say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
-            os.system("wine say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
+def say_single_message(message):
+    session = str(uuid.uuid1())
+    temp_wav = os.path.join(TMP_DIR, session+'.wav')
+    message = message.replace('"', '').replace('&', '').replace('|', '').replace(';', '')
+    if os.name == 'nt':
+        #print("say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
+        os.system("say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
+    else:
+        #print("wine say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
+        os.system("wine say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
+    wave_obj = sa.WaveObject.from_wave_file(temp_wav)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+    os.remove(temp_wav)
 
-        wave_obj = sa.WaveObject.from_wave_file(temp_wav)
-        play_obj = wave_obj.play()
-        play_obj.wait_done()
-        os.remove(temp_wav)
-    except Exception:
-        pass
+def run_singlethread():
+    while True:
+        try:
+            resp = sock.recv(2048).decode('utf-8')
+            message = resp.split(':',2)[2]
+            say_single_message(message)
+            
+        except Exception:
+            pass
 
-sock.close()
+    sock.close()
+
+def run_queue_single():
+    
+    while True:
+        try:
+            resp = sock.recv(2048).decode('utf-8')
+            for line in resp.split('\n'):
+                message = line.split(':',2)[2]
+                say_single_message(message)
+            
+        except Exception:
+            pass
+
+if __name__ ==  '__main__':
+
+    if MODE == 'keepup':
+        run_singlethread()
+    elif MODE == 'queue':
+        run_queue_single()
+    else:
+        print('Please select a mode by editing the MODE variable ')
