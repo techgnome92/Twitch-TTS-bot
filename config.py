b7639c190faf1da2963a3f951a221ad318900d65
Frode <frodeluren@gmail.com>
2020-11-07 22:25:02 +0100

    Moves settings-variables to config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+settings = dict(
+    server = 'irc.chat.twitch.tv',
+    port = 6667,
+    nickname = '', #The bots nickname
+    token = 'oauth:', #Get it from https://twitchapps.com/tmi/
+    channel = '#', #Channel username like '#moistcr1tikal'
+
+    # 'keepup'  Keeping up with recent messages, some messages might be skipped.
+    # 'queue'   Queuing up messages and says every message.
+    MODE = 'queue',
+
+    TMP_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tmp')
+)
