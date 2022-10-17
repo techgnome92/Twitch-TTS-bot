e8d6947e4f9d36be0ad9744405635ea15e6715bf
Frode Pedersen <frodeluren@gmail.com>
2022-10-17 23:53:18 +0200

    Answers PONG when receiving PING message from twitch. Uses traceback on exception.

--- dectalktwitchtts.py
+++ dectalktwitchtts.py
@@ -3,6 +3,7 @@ from pydub import AudioSegment
 import simpleaudio as sa
 import config
 import threading
+import traceback
 
 # Getting variables from config.py
 server = config.settings['server']
@@ -27,60 +28,69 @@ resp = sock.recv(2048).decode('utf-8')
 resp = sock.recv(2048).decode('utf-8')
 
 def say_single_message(message):
-    session = str(uuid.uuid1())
-    temp_wav = os.path.join(TMP_DIR, session+'.wav')
-    message = message.replace('"', '').replace('&', '').replace('|', '').replace(';', '')
-    if os.name == 'nt':
-        #print("say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
-        os.system("say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
-    else:
-        #print("wine say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
-        os.system("wine say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
-    wave_obj = sa.WaveObject.from_wave_file(temp_wav)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-    os.remove(temp_wav)
+    try:
+        session = str(uuid.uuid1())
+        temp_wav = os.path.join(TMP_DIR, session+'.wav')
+        message = message.replace('"', '').replace('&', '').replace('|', '').replace(';', '')
+        if os.name == 'nt':
+            #print("say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
+            os.system("say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
+        else:
+            #print("wine say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
+            os.system("wine say.exe -d dtalk_us.dic -w "+temp_wav+" \"[:phoneme on]"+message+"\"")
+        wave_obj = sa.WaveObject.from_wave_file(temp_wav)
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
+        os.remove(temp_wav)
+    except Exception:
+        traceback.print_exc()
+
+def is_valid_line(line):
+    if len(line) and not line.startswith("PING"):
+        username = line.split(':',1)[1].split('!', 1)[0]
+        if username not in IGNORE_LIST:
+            return true
+        else:
+            return false
+    elif line.startswith("PING"):
+        sock.send(bytes("PONG :tmi.twitch.tv", "UTF-8"))
+        return false
 
 def run_singlethread():
     while True:
         try:
             resp = sock.recv(2048).decode('utf-8')
-            line = resp.split('\n')[0]
-            username = line.split(':',1)[1].split('!', 1)[0]
-            if username not in IGNORE_LIST:
+            line = resp.split('\r\n')[0]
+            if is_valid_line(line):
                 message = line.split(':',2)[2]
                 say_single_message(message)
             
         except Exception:
-            pass
-
-    sock.close()
+            traceback.print_exc()
 
 def run_queue_single():
     while True:
         try:
             resp = sock.recv(2048).decode('utf-8')
-            for line in resp.split('\n'):
-                username = line.split(':',1)[1].split('!', 1)[0]
-                if username not in IGNORE_LIST:
+            for line in resp.split('\r\n'):
+                if is_valid_line(line):
                     message = line.split(':',2)[2]
                     say_single_message(message)
             
         except Exception:
-            pass
+            traceback.print_exc()
 
 def run_multithread():
     while True:
         try:
             resp = sock.recv(2048).decode('utf-8')
-            for line in resp.split('\n'):
-                username = line.split(':',1)[1].split('!', 1)[0]
-                if username not in IGNORE_LIST:
+            for line in resp.split('\r\n'):
+                if is_valid_line(line):
                     message = line.split(':',2)[2]
                     threading.Thread(target=say_single_message, args=(message,)).start()
-            
+
         except Exception:
-            pass
+            traceback.print_exc()
 
 if __name__ ==  '__main__':
     if MODE == 'keepup':
