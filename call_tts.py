28321fc9f4ca86d179ae1cd95adb67858fd6907f
TechGnome <techgnome92@gmail.com>
2025-03-09 23:21:27 -0700

    fix misspelling of dectalk

--- call_tts.py
+++ call_tts.py
@@ -6,13 +6,13 @@ voices = {
     "ms_mary": "MSMary",
     "ms_david": "TTS_MS_EN-US_DAVID_11.0",
     "ms_zira": "TTS_MS_EN-US_ZIRA_11.0",
-    "dektalk": "dektalk"
+    "dectalk": "dectalk"
 }
 
 
 def create_wave(tmp_dir: str, message: str, tts: str = "ms_sam"):
 
-    if tts == "dektalk":
+    if tts == "dectalk":
         cmd_line = f"say.exe -d dtalk_us.dic -w {tmp_dir} [:phoneme on] '{message}'"
         if os.name != "nt":
             cmd_line = f"wine {cmd_line}"
